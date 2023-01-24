ce08995b34f028fba4d64afff43d7dca0734b97e
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-01-24 17:43:25 +0000

    commit

--- MathFunctions.py
+++ MathFunctions.py
@@ -12,7 +12,7 @@ def plot_cosine(self, *args):
     x = np.linspace(0, 10, 550)
     y = np.cos(x)
 
-    plot = MeshLinePlot(color=[0, 0, 1, 1])
+    plot = MeshLinePlot(color=[239/255, 103/255, 76/255, 1])
     plot.points = [(i, j) for i, j in zip(x, y)]
     self.graph.add_plot(plot)
 
@@ -21,7 +21,25 @@ def plot_sine(self, *args):
     x = np.linspace(0, 10, 550)
     y = np.sin(x)
 
-    plot = MeshLinePlot(color=[0, 0, 1, 1])
+    plot = MeshLinePlot(color=[74/255, 188/255, 112/255, 1])
+    plot.points = [(i, j) for i, j in zip(x, y)]
+    self.graph.add_plot(plot)
+
+
+def plot_sqrt(self, *args):
+    x = np.linspace(0, 10, 500)
+    y = np.sqrt(x)
+
+    plot = MeshLinePlot(color=[246/255, 136/255, 40/255, 1])
+    plot.points = [(i, j) for i, j in zip(x, y)]
+    self.graph.add_plot(plot)
+
+
+def plot_exp(self, *args):
+    x = np.linspace(0, 10, 1000)
+    y = np.exp(x)
+
+    plot = MeshLinePlot(color=[177/255, 96/255, 218/255, 1])
     plot.points = [(i, j) for i, j in zip(x, y)]
     self.graph.add_plot(plot)
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.uix.label import Label
 from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
@@ -6,52 +7,58 @@ from kivymd.theming import ThemeManager
 from kivymd.uix.button import MDFlatButton
 from kivy_garden.graph import Graph, MeshLinePlot
 import numpy as np
+from MathFunctions import plot_sine, plot_exp, plot_sqrt, plot_cosine
+from kivymd.uix.menu import MDDropdownMenu
 
 
 class PlotWidget(Screen):
     def __init__(self, **kwargs):
         super(PlotWidget, self).__init__(**kwargs)
+
         self.graph = Graph(xlabel='X', ylabel='Y', x_ticks_minor=5,
                            x_ticks_major=25, y_ticks_major=1,
                            y_grid_label=True, x_grid_label=True, padding=5,
-                           x_grid=True, y_grid=True, xmin=-0, xmax=10, ymin=-1, ymax=1)
-
-        self.sin_button = MDFlatButton(text='sin', pos_hint={"center_x": 0.5, "center_y": .03}, on_press=self.plot_sine)
-        self.cos_button = MDFlatButton(text='cos', pos_hint={"center_x": 0.7, "center_y": .03},
-                                       on_press=self.plot_cosine)
-        self.sqrt_button = MDFlatButton(text='sqrt', pos_hint={"center_x": 0.3, "center_y": .03},
-                                        on_press=self.plot_sqrt)
-        self.exp_button = MDFlatButton(text='exp', pos_hint={"center_x": 0.9, "center_y": .03},
-                                        on_press=self.plot_sqrt)
+                           x_grid=True, y_grid=True, xmin=-1, xmax=15, ymin=-1, ymax=10,
+                           size_hint=(.8, .8), pos_hint={'center_x': .5, 'center_y': .5},
+                           font_size='14sp',
+                           background_color=[0, 0, 0, 1])
         self.add_widget(self.graph)
-        self.add_widget(self.sin_button)
-        self.add_widget(self.cos_button)
-        self.add_widget(self.sqrt_button)
-        self.add_widget(self.exp_button)
 
-    def plot_cosine(self, *args):
-        x = np.linspace(0, 2 * np.pi, 500)
-        y = np.cos(x)
+        # Function button
+        button_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.1), pos_hint={'center_x': 0.5, 'y': 0.03})
+        self.sin_button = MDFlatButton(text='sin', on_press=self.plot_sine)
+        button_layout.add_widget(self.sin_button)
+        self.cos_button = MDFlatButton(text='cos', on_press=self.plot_cosine)
+        button_layout.add_widget(self.cos_button)
+        self.exp_button = MDFlatButton(text='exp', on_press=self.plot_exp)
+        button_layout.add_widget(self.exp_button)
+        self.sqrt_button = MDFlatButton(text='sqrt', on_press=self.plot_sqrt)
+        button_layout.add_widget(self.sqrt_button)
 
-        plot = MeshLinePlot(color=[0, 1, 0, 1])
-        plot.points = [(i, j) for i, j in zip(x, y)]
-        self.graph.add_plot(plot)
+        self.add_widget(button_layout)
 
-    def plot_sqrt(self, *args):
-        x = np.linspace(0, 10, 500)
-        y = np.sqrt(x)
-
-        plot = MeshLinePlot(color=[0, 0, 1, 1])
-        plot.points = [(i, j) for i, j in zip(x, y)]
-        self.graph.add_plot(plot)
+        # the clear button
+        self.clear_button = MDFlatButton(text='undo', on_press=self.clear_plots)
+        button_layout.add_widget(self.clear_button)
+        for i in range(1,4):
+            label = Label(text=str(i), pos_hint={'x':0,'top':1-i/4}, font_size='12sp')
+            self.add_widget(label)
 
     def plot_sine(self, *args):
-        x = np.linspace(0, 10, 550)
-        y = np.sin(x)
+        plot_sine(self)
 
-        plot = MeshLinePlot(color=[0, 0, 1, 1])
-        plot.points = [(i, j) for i, j in zip(x, y)]
-        self.graph.add_plot(plot)
+    def plot_cosine(self, *args):
+        plot_cosine(self)
+
+    def plot_exp(self, *args):
+        plot_exp(self)
+
+    def plot_sqrt(self, *args):
+        plot_sqrt(self)
+
+    def clear_plots(self, *args):
+        for plot in self.graph.plots:
+            self.graph.remove_plot(plot)
 
 
 class PlotApp(MDApp):
